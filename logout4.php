96dbf93098d77fd192dddbee0e01ef133b289f8f
idisi <siderida@hotmail.de>
2014-04-04 18:48:58 +0200

    hallo
    Also ich habe es mit dem Login und Logout geschafft ,und ich habe es
    auch als button erstellt:).Ich habe versucht einbischen mit dem
    Verschlüsselung weiter zu machen ,aber leider bis jetzt nicht so vie
    geschafft.

---
+++ logout4.php
@@ -0,0 +1,22 @@
+
+<form action=login.php method=post>
+   
+    
+                       
+     <?php
+    // session_start();
+    // unset($_SESSION['username']);
+     
+     if(isset($_POST["abmelden"])){
+         session_destroy();
+     
+     //header("Location: login.php");
+     //exit
+         
+     }
+   
+    
+?>   
+                   
+</form>                      
+    
