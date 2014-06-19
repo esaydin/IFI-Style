a46a961a7a6e10e68b4477bddc6d3f84768ea317
esaydin <esranur89@hotmail.de>
2014-06-19 21:33:03 +0200

    Kalender hinzugefügt

--- web/impressum.php
+++ web/impressum.php
@@ -5,19 +5,66 @@
 <!--Import der Klasse header.php  -->
 <?php include_once 'header.php'; ?>
 <!--Inhalte der Startseite -->
-<div id="inhalt">
+<div id="inhaltimpressum">
     <div id = "InhaltHöhe">
         <h1 class="studh2">Ihr SearchProStud Team!</h1>
-        
     </div>
+    
+    <h2>Kontakt:</h2>
+    
+    <table>
+        <tr>
+            <td>Foto</td>
+            <td><img src="bilder/esra.jpg" id="team"/></td>
+        </tr>
+        <tr>
+            <td>Name:</td>
+            <td>Esra Nur Aydin</td>
+        </tr>
+        <tr>
+            <td>Matrikelnummer:</td>
+            <td>303925</td>
+        </tr>
+        <tr>
+            <td>E-Mail:</td>
+            <td>esaydin@stud.hs-bremen.de</td>
+        </tr>
+    </table>
+     <table id="guelcin">
+        <tr>
+            <td><img src="bilder/gülcin.jpg" id="team"/></td>
+        </tr>
+        <tr> 
+            <td>Gülcin Gürel</td>
+        </tr>
+        <tr> 
+            <td>307021</td>
+        </tr>
+        <tr>
+            <td>gguerel@stud.hs-bremen.de</td>
+        </tr>
+    </table>
+    <table id="huedi">
+        <tr>
+            <td><img src="bilder/huedi.jpg" id="team"/></td>
+        </tr>
+        <tr> 
+            <td>Hüdanur Sarar</td>
+        </tr>
+        <tr> 
+            <td>28208</td>
+        </tr>
+        <tr>
+            <td>hsarar@stud.hs-bremen.de</td>
+        </tr>
+    </table>
+    
+    
+
+
+
 </div>
 
-<div id="info">   
-    <div id = "InhaltHöhe">
-      
-    
-    </div>
-</div>
 <?php include_once 'footer.php'; ?>
 
 
